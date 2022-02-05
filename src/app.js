c5e2bcde85435ec72e116dbe08e1180a53084a45
johmert <johmert@users.noreply.github.com>
2022-02-05 04:11:37 -0800

    static data practice

--- src/app.js
+++ src/app.js
@@ -1,7 +1,49 @@
 const express = require("express");
 const app = express();
+const flips = require("./data/flips-data");
+const counts = require("./data/counts-data");
 
-// TODO: Follow instructions in the checkpoint to implement ths API.
+app.use(
+  "/counts/:countId",
+  (req, res, next) => {
+    const { countId } = req.params;
+    const foundCount = counts[countId];
+
+    if(foundCount === undefined) {
+      next(`Count id not found: ${countId}`);
+    } else {
+      res.json({ data: foundCount });
+    }
+  }
+);
+
+app.use(
+  "/counts",
+  (req, res) => {
+    res.json({ data: counts });
+  }
+);
+
+app.use(
+  "/flips/:flipId",
+  (req, res, next) => {
+    const { flipId } = req.params;
+    const foundFlip = flips.find(flip => flip.id === Number(flipId));
+
+    if(foundFlip){
+      res.json({ data: foundFlip });
+    } else {
+      next(`Flip id not found ${flipId}`);
+    }
+  }
+);
+
+app.use(
+  "/flips",
+  (req, res) => {
+    res.json({ data: flips });
+  }
+);
 
 // Not found handler
 app.use((request, response, next) => {
