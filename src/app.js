5b89c57450dffb9da8e409cbd233d0ac0eb6114b
johmert <johmert@users.noreply.github.com>
2022-02-06 20:03:21 -0800

    restful API practice

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const app = express();
 const flips = require("./data/flips-data");
 const counts = require("./data/counts-data");
 
+app.use(express.json());
+
 app.use(
   "/counts/:countId",
   (req, res, next) => {
@@ -38,12 +40,30 @@ app.use(
   }
 );
 
-app.use(
+app.get(
   "/flips",
   (req, res) => {
     res.json({ data: flips });
   }
 );
+
+//Variable to hold next ID
+//Because some IDs may already be used, find the largest assigned ID
+let lastFlipId = flips.reduce((maxId, flip) => Math.max(maxId, flip.id), 0);
+
+app.post("/flips", (req, res, next) => {
+  const { data: { result } = {} } = req.body;
+  if(result) {
+    const newFlip = {
+    id: ++lastFlipId, //increment last ID, then assign as the currentID
+    result,
+    };
+    flips.push(newFlip);
+    counts[result] = counts[result] +1;
+  } else { 
+    res.status(201).json({ data: newFlip });
+  }
+});
 
 // Not found handler
 app.use((request, response, next) => {
