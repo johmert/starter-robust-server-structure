951078c4dcbe003c9d7978b23f20ddabfeffb013
johmert <johmert@users.noreply.github.com>
2022-02-08 01:10:36 -0800

    organizing express code

--- src/app.js
+++ src/app.js
@@ -1,94 +1,43 @@
 const express = require("express");
 const app = express();
+
 const flips = require("./data/flips-data");
 const counts = require("./data/counts-data");
-const res = require("express/lib/response");
+
+const flipsRouter = require("./flips/flips.router");
 
 app.use(express.json());
 
-app.use(
-  "/counts/:countId",
-  (req, res, next) => {
-    const { countId } = req.params;
-    const foundCount = counts[countId];
+app.get("/counts/:countId", (request, response, next) => {
+  const { countId } = request.params;
+  const foundCount = counts[countId];
 
-    if(foundCount === undefined) {
-      next({status: 404, message: `Count id not found: ${countId}`});
-    } else {
-      res.json({ data: foundCount });
-    }
+  if (foundCount === undefined) {
+    next({
+      status: 404,
+      message: `Count id not found: ${countId}`,
+    });
+  } else {
+    response.json({ data: foundCount });
   }
-);
+});
 
-app.use(
-  "/counts",
-  (req, res) => {
-    res.json({ data: counts });
-  }
-);
+app.get("/counts", (request, response) => {
+  response.json({ data: counts });
+});
 
-app.use(
-  "/flips/:flipId",
-  (req, res, next) => {
-    const { flipId } = req.params;
-    const foundFlip = flips.find(flip => flip.id === Number(flipId));
-
-    if(foundFlip){
-      res.json({ data: foundFlip });
-    } else {
-      next({status: 404, message: `Flip id not found ${flipId}`});
-    }
-  }
-);
-
-app.get(
-  "/flips",
-  (req, res) => {
-    res.json({ data: flips });
-  }
-);
-
-//Ne middleware function to validate the request body
-function bodyHasResultProperty(req, res, next){
-  const { data: { result } = {} } = req.body;
-  if(result){
-    return next(); // Call next() without an error message if the result exists
-  }
-  next({
-    status: 400,
-    message: "A 'result' property is required."
-  });
-} 
-
-//Variable to hold next ID
-//Because some IDs may already be used, find the largest assigned ID
-let lastFlipId = flips.reduce((maxId, flip) => Math.max(maxId, flip.id), 0);
-
-app.post(
-  "/flips",
-  bodyHasResultProperty, // Add validation middleware function
-  (req, res) => {
-    // Route handler no longer has validation code.
-    const { data: { result } = {} } = req.body;
-    const newFlip = {
-      id: ++lastFlipId, //increment last ID, then assign as the currentID
-      result,
-    };
-    flips.push(newFlip);
-    res.status(201).json({ data: newFlip });
-  }
-);
+app.use("/flips", flipsRouter);
 
 // Not found handler
 app.use((request, response, next) => {
-  next(`Not found: ${request.originalUrl}`);
+  next({ status: 404, message: `Not found: ${request.originalUrl}` });
 });
 
 // Error handler
 app.use((error, request, response, next) => {
   console.error(error);
   const { status = 500, message = "Something went wrong!" } = error;
-  res.status(status).json({ error: message });
+  response.status(status).json({ error: message });
 });
 
 module.exports = app;
